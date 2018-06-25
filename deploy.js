ad503b9dd43e6bdd9ee522d71163be6a6d9d5fad
acrylix <1619025+acrylix@users.noreply.github.com>
2018-06-25 15:58:43 -0400

    react front end (basic)

--- deploy.js
+++ deploy.js
@@ -16,9 +16,10 @@ const deploy = async () => {
   console.log(accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: '0x'+bytecode, arguments: ['Hello'] })
+    .deploy({ data: '0x' + bytecode })
     .send({ gas: '1000000', from: accounts[0] });
 
+  console.log(interface);
   console.log('Contract deployed to', result.options.address);
 };
 deploy();
